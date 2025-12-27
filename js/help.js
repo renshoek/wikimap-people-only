d3b2f973dc28cc3bbeb5940f2cbd6822720e295d
Rens <58260544+renshoek@users.noreply.github.com>
2025-12-27 13:35:32 +0100

    Refactor Shepherd tour and network initialization
    Refactor the Shepherd tour setup and network initialization. Added global options and improved node handling.

--- js/help.js
+++ js/help.js
@@ -1,128 +1,159 @@
-/* global Shepherd */
-const isTouchDevice = 'd' in document.documentElement;
+/* global vis, bindNetwork, getNormalizedId, wordwrap, getColor, noInputDetected, getItems, addItem, clearItems, unlockAll, fetchPageTitle, getRandomArticle, networkFromJson */ // eslint-disable-line max-len
+// This script contains the code that creates the central network, as well as
+// a function for resetting it to a brand new page.
 
-// Create the Shepherd tour
 
-const buttons = document.getElementById('buttons');
-const formbox = document.getElementById('formbox');
+let nodes;
+let edges;
+let network; // Global variables
 
-const shepherd = new Shepherd.Tour({
-  defaults: {
-    classes: 'shepherd-theme-arrows',
-    showCancelLink: true,
+window.startpages = [];
+// Tracks whether the network needs to be reset. Used to prevent deleting nodes
+// when multiple nodes need to be created, because AJAX requests are async.
+
+const container = document.getElementById('container');
+// Global options
+const options = {
+  nodes: {
+    shape: 'dot',
+    scaling: {
+      min: 20,
+      max: 30,
+      label: { min: 14, max: 30, drawThreshold: 9, maxVisible: 20 },
+    },
+    font: { size: 14, face: getComputedStyle(document.body).fontFamily },
   },
-});
-
-// Add steps to the Shepherd tour.
-
-shepherd.addStep({
-  text: [
-    'Input the name of a Wikipedia article here.',
-    'You can compare multiple topics by pressing <kbd>,</kbd> ' +
-    '<kbd>Tab</kbd> or <kbd>Enter</kbd> after each one.',
-  ],
-  attachTo: '#input bottom',
-  buttons: [
-    {
-      text: 'Back',
-      classes: 'shepherd-button-secondary',
-      action: shepherd.back,
-    },
-    {
-      text: 'Next',
-      classes: 'shepbtn',
-      action: shepherd.next,
-    },
-  ],
-});
-
-shepherd.addStep({
-  text: [
-    "Once you're done, submit your query.",
-    'Wikipedia Map will create a node for each input topic.',
-  ],
-  attachTo: '#submit bottom',
-  buttons: [
-    {
-      text: 'Back',
-      classes: 'shepherd-button-secondary',
-      action: shepherd.back,
-    },
-    {
-      text: 'Next',
-      classes: 'shepbtn',
-      action: shepherd.next,
-    },
-  ],
-  tetherOptions: {
-    attachment: 'top left',
-    targetAttachment: 'bottom center',
-    offset: '0px -35px',
+  interaction: {
+    hover: true,
+    hoverConnectedEdges: false,
+    selectConnectedEdges: true,
   },
-});
-
-shepherd.addStep({
-  text: [
-    'Click a node to expand it.',
-    'Expanding a node creates a new node for each Wikipedia article linked in the first paragraph of the article whose node you clicked.',
-    '<img src="https://images.prismic.io/luke/db049805-b070-43c5-a412-d44c5ac3a4d7_wikipedia-expand.gif" alt="Expanding a Wikipedia Map node" style="width: 410px; height: 410px;" />',
-  ],
-  buttons: [
-    {
-      text: 'Back',
-      classes: 'shepherd-button-secondary',
-      action: shepherd.back,
+  physics: {
+    barnesHut: {
+      gravitationalConstant: -2000,
+      springConstant: 0.02, // Lower value = looser springs
+      springLength: 300,    // Longer value = longer edges
     },
-    {
-      text: 'Next',
-      classes: 'shepbtn',
-      action: shepherd.next,
+    stabilization: {
+      iterations: 2500,
     },
-  ],
-});
+  },
+};
 
-shepherd.addStep({
-  text: [
-    'Keep expanding nodes to build a map and connect topics!',
-  ],
-  buttons: [
-    {
-      text: 'Back',
-      classes: 'shepherd-button-secondary',
-      action: shepherd.back,
-    },
-    {
-      text: "Let's go!",
-      classes: 'shepbtn',
-      action: shepherd.next,
-    },
-  ],
-});
+nodes = new vis.DataSet();
+edges = new vis.DataSet();
+let data = { nodes, edges };
+let initialized = false;
 
-// Take away the info box when the tour has started...
-shepherd.on('start', () => {
-  document.getElementById('container').style.opacity = 0.3;
-  document.getElementById('container').style.pointerEvents = 'none';
-  formbox.style.opacity = 0.3;
-  buttons.style.opacity = 0.3;
-});
 
-// ... and bring it back when the tour goes away
-function opaque() {
-  document.getElementById('container').style.opacity = '';
-  document.getElementById('container').style.pointerEvents = '';
-  formbox.style.opacity = 1;
-  buttons.style.opacity = 1;
+// Set up the network
+function makeNetwork() {
+  if (initialized) throw new Error('Network is already initialized');
+  network = new vis.Network(container, data, options);
+  bindNetwork();
+
+  window.startpages = [];
+  window.tracenodes = [];
+  window.traceedges = [];
+  nodes = new vis.DataSet();
+  edges = new vis.DataSet();
+  data = { nodes, edges };
+  network.setData(data);
+
+  initialized = true;
 }
-shepherd.on('complete', () => {
-  opaque();
-  document.querySelector('#input input').focus();
-});
-shepherd.on('cancel', opaque);
 
-// Prompt user for input when none detected
-function noInputDetected() {
-  document.getElementById('container').style.opacity = 0.3;
-  buttons.style.opacity = 0.3;
-  shepherd.show();
+// Get the object to represent a "start node" for a given page name
+const getStartNode = pageName => ({
+  id: getNormalizedId(pageName),
+  label: wordwrap(decodeURIComponent(pageName), 20),
+  value: 2,
+  level: 0,
+  color: getColor(0),
+  x: 0,
+  y: 0,
+  parent: getNormalizedId(pageName), // Parent is self
+});
+
+// Reset everything to its initial state
+function clearNetwork() {
+  window.startpages = [];
+  window.tracenodes = [];
+  window.traceedges = [];
+  nodes = new vis.DataSet();
+  edges = new vis.DataSet();
+  data = { nodes, edges };
+  network.setData(data);
+
+  const cf = document.getElementById('input');
+  unlockAll(cf);
+}
+
+// Add and remove "start nodes" to make the list of start nodes match the list passed
+function setStartPages(starts) {
+  const newStartPages = starts.map(getNormalizedId);
+  if (!initialized) makeNetwork();
+  const toRemove = window.startpages.filter(id => !newStartPages.includes(id));
+  const toAdd = starts.filter((pageName, i) => !window.startpages.includes(newStartPages[i]));
+
+  nodes.remove(toRemove);
+  nodes.add(toAdd.map(getStartNode));
+  window.startpages = newStartPages;
+}
+
+
+// Reset the network with the content from the input box.
+function go() {
+  // Get items entered
+  const cf = document.getElementById('input');
+  const inputs = getItems(cf);
+  // If no input is given, prompt user to enter articles
+  if (!inputs[0]) {
+    noInputDetected();
+    return;
+  }
+
+  Promise.all(inputs.map(fetchPageTitle))
+    .then((pageTitles) => {
+      // Record on the commafield item which node the input corresponds to
+      pageTitles.forEach((pageTitle, i) => {
+        cf.getElementsByClassName('item')[i].dataset.nodeId = getNormalizedId(pageTitle);
+      });
+      // Make the network‘s start pages the pages from the inputs
+      setStartPages(pageTitles);
+    });
+
+  // Show 'clear' button
+  document.getElementById('clear').style.display = '';
+}
+
+
+// Reset the network with one or more random pages.
+function goRandom() {
+  const cf = document.getElementsByClassName('commafield')[0];
+  getRandomArticle().then((ra) => {
+    addItem(cf, decodeURIComponent(ra));
+    go();
+  });
+}
+
+// Reset the network with content from a JSON string
+function resetNetworkFromJson(j) {
+  if (!initialized) makeNetwork();
+  clearNetwork();
+  const obj = networkFromJson(j);
+  nodes = obj.nodes;
+  edges = obj.edges;
+  window.startpages = obj.startpages;
+  // Fill the network
+  network.setData({ nodes, edges });
+  // Populate the top bar
+  const cf = document.getElementById('input');
+  clearItems(cf);
+  window.startpages.forEach((sp) => {
+    console.log(sp, nodes.get(sp));
+    addItem(cf, nodes.get(sp).label.replace(/\s+/g, ' '));
+    // TODO: set node IDs on commafield items
+    // TODO: lock commafield items that have been expanded
+  });
 }
